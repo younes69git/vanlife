19cac8ecde68cc1285741982f81e0e784ed6d285
Younes Zepeek <youneszepeek23@gmail.com>
2025-07-08 18:29:06 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
+import img from "../../public/image 53.png"
 const Home = () => {
   return ( 
     <>
-      <main className="content">
+      <main className="content"style={{  backgroundImage: `url(${img})`}}>
         <div className="container">
           <h1>You got the travel plans, we got the travel vans.</h1>
           <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
